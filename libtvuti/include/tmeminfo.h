a409859db896d1c99bbb68999e620fc787a4c9c5
Robert Höhne <robert.hoehne@gmx.net>
1998-09-16 17:32:58 +0000

    Changes to allow the use of shorter memory statistics Make the get_mem_info virtual

--- libtvuti/include/tmeminfo.h
+++ libtvuti/include/tmeminfo.h
@@ -6,11 +6,14 @@
 class TIDEMemInfo : public TView
 {
 public:
-  TIDEMemInfo(const TRect & r) : TView(r), p_mem(0), v_mem(0) {};
+  TIDEMemInfo(const TRect & r, int _long_format = 0)
+    : TView(r), p_mem(0), v_mem(0), long_format(_long_format) {};
   void update(int force = 0);
   virtual void draw();
+  virtual void get_mem_info(unsigned long & virt_mem,unsigned long & phys_mem);
 private:
   unsigned long p_mem,v_mem;
+  int long_format;
 };
 
 void get_mem_info(unsigned long &,unsigned long &);

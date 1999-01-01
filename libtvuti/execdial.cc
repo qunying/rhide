acfd335d2f994692e1931a5bb68804a979afcc04
Robert Höhne <robert.hoehne@gmx.net>
1999-01-01 13:14:18 +0000

    Provide the execDialog() function.

---
+++ libtvuti/execdial.cc
@@ -0,0 +1,25 @@
+#define Uses_TDialog
+#define Uses_TProgram
+#define Uses_TApplication
+#define Uses_TDeskTop
+
+#define Uses_tvutilFunctions
+#include <libtvuti.h>
+
+ushort execDialog( TDialog *d, void *data )
+{
+ TView *p=TProgram::application->validView( d );
+ if (p==0)
+    return cmCancel;
+ else
+  {
+   if (data!=0)
+      p->setData(data);
+   ushort result=TProgram::deskTop->execView(p);
+   if (result!=cmCancel && data!=0)
+      p->getData(data);
+   destroy(p);
+   return result;
+  }
+}
+

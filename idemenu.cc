48cf77637b9d509009726b12f0a218f5eb5c53e3
Robert Höhne <robert.hoehne@gmx.net>
1999-01-01 13:19:45 +0000

    Use now the extended key-codes.

--- idemenu.cc
+++ idemenu.cc
@@ -2,6 +2,7 @@
 /* This file is part of RHIDE. */
 #define Uses_TRect
 #define Uses_TKeys
+#define Uses_TKeys_Extended
 #define Uses_IDEConst
 #define Uses_TMenuBar
 #define Uses_TSubMenu
@@ -22,6 +23,8 @@
 #define Uses_TCEditor_Commands
 #define Uses_TCEditor_External
 #include <ceditor.h>
+
+#define kbNoKey (ushort)0
 
 // This is a little trick to reduce the size
 
@@ -71,23 +74,23 @@ TMenuBar *IDE::initMenuBar( TRect r )
   r = TProgram::application->getExtent();
   r.b.y = r.a.y + 1;
 
-  TMenuItem &syntax_sub = *new TSubMenu(_("~S~yntax help"), kbAltS, hcSyntax)
-    +*new _TMenuItem( __("~S~yntax help"), cmSyntaxHelp, kbCtrlF1, hcSyntaxHelp, "Ctrl+F1")
+  TMenuItem &syntax_sub = *new TSubMenu(_("~S~yntax help"), kbAlS, hcSyntax)
+    +*new _TMenuItem( __("~S~yntax help"), cmSyntaxHelp, kbCtF1, hcSyntaxHelp, "Ctrl+F1")
     +*new _TMenuItem( __("~F~iles to search"), cmSyntaxFiles, kbNoKey, hcSyntaxFiles)
     +*new _TMenuItem( __("~O~ptions for searching"), cmSyntaxOptions, kbNoKey, hcSyntaxOptions)
   ;
 
-  TSubMenu &help_menu = *new TSubMenu( _("~H~elp"), kbAltH , hcHelpMenu )
+  TSubMenu &help_menu = *new TSubMenu( _("~H~elp"), kbAlH , hcHelpMenu )
     +*new _TMenuItem( __("~H~elp"), cmHelp, kbF1, hcHelp, "F1" )
-    +*new _TMenuItem( __("RHIDE Help ~i~ndex"), cmHelpIndex, kbShiftF1, hcHelpIndex, "Shift+F1" )
+    +*new _TMenuItem( __("RHIDE Help ~i~ndex"), cmHelpIndex, kbShF1, hcHelpIndex, "Shift+F1" )
     +     syntax_sub
     +*new _TMenuItem( __("Index for s~y~ntax help"), cmSyntaxIndex, kbNoKey, hcSyntaxIndex)
     +*new _TMenuItem( __("lib~c~ reference"), cmLibcHelp, kbNoKey, hcLibcHelp )
     +*new _TMenuItem( __("Help ~a~bout help"), cmHelpHelp, kbNoKey, hcHelpHelp )
-    +*new _TMenuItem( __("~L~ast help"), cmLastHelp, kbAltF1, hcLastHelp, "Alt+F1" )
+    +*new _TMenuItem( __("~L~ast help"), cmLastHelp, kbAlF1, hcLastHelp, "Alt+F1" )
   ;
 
-  TSubMenu& file_menu = *new TSubMenu( _("~F~ile"), kbAltF, hcFile )
+  TSubMenu& file_menu = *new TSubMenu( _("~F~ile"), kbAlF, hcFile )
     +*new _TMenuItem( __("~O~pen"), cmOpenEditor, kbF3, hcOpenEditor, "F3" )
     +*new _TMenuItem( __("~N~ew"), cmNewEditor, kbNoKey, hcNewEditor )
     +*new _TMenuItem( __("~S~ave"), cmSaveEditor, kbF2, hcSaveEditor, "F2" )
@@ -102,29 +105,29 @@ TMenuBar *IDE::initMenuBar( TRect r )
 #else
     +*new _TMenuItem( __("S~h~ell"), cmDosShell, kbNoKey, hcDosShell )
 #endif
-    +*new _TMenuItem( __("E~x~it"), cmQuit, kbAltX, hcQuit, "Alt+X" )
+    +*new _TMenuItem( __("E~x~it"), cmQuit, kbAlX, hcQuit, "Alt+X" )
   ;
 
-  TMenuItem& macro_sub = *new TSubMenu( _("~M~acro"), kbAltM, hcMacro )
-    +*new _TMenuItem( __("~R~ecord"), cmcRecordMacro, kbShiftF10, hcRecord, "Shift+F10" )
-    +*new _TMenuItem( __("~S~top"), cmcStopMacro, kbAltF10, hcStop, "Alt+F10" )
-    +*new _TMenuItem( __("~P~lay"), cmcPlayMacro, kbCtrlF10, hcPlay, "Ctrl+F10" )
+  TMenuItem& macro_sub = *new TSubMenu( _("~M~acro"), kbAlM, hcMacro )
+    +*new _TMenuItem( __("~R~ecord"), cmcRecordMacro, kbShF10, hcRecord, "Shift+F10" )
+    +*new _TMenuItem( __("~S~top"), cmcStopMacro, kbAlF10, hcStop, "Alt+F10" )
+    +*new _TMenuItem( __("~P~lay"), cmcPlayMacro, kbCtF10, hcPlay, "Ctrl+F10" )
     +     newLine()
     +*new _TMenuItem( __("~C~hoose"), cmcChooseMacro, kbNoKey, hcChooseMacro)
-    +*new _TMenuItem( __("R~e~peat"), cmcRepeatMacro, kbShiftF3, hcRepeatMacro, "Shift+F3")
+    +*new _TMenuItem( __("R~e~peat"), cmcRepeatMacro, kbShF3, hcRepeatMacro, "Shift+F3")
     +*new _TMenuItem( __("~G~enerate Code"), cmcGenCodeForMacro, kbNoKey, hcGenCodeForMacro)
   ;
 
-  TSubMenu& edit_menu = *new TSubMenu( _("~E~dit"), kbAltE, hcEdit )
-    +*new _TMenuItem( __("~U~ndo"), cmcUndo, kbAltBack, hcUndo, "Alt+Backspace" )
+  TSubMenu& edit_menu = *new TSubMenu( _("~E~dit"), kbAlE, hcEdit )
+    +*new _TMenuItem( __("~U~ndo"), cmcUndo, kbAlBackSpace, hcUndo, "Alt+Backspace" )
     +*new _TMenuItem( __("~R~edo"), cmcRedo, kbNoKey, hcRedo )
     +     newLine()
-    +*new _TMenuItem( __("Cu~t~"), cmcCut, kbShiftDel, hcCut, "Shift+Del" )
-    +*new _TMenuItem( __("~C~opy"), cmcCopy, kbCtrlIns, hcCopy, "Ctrl+Ins" )
-    +*new _TMenuItem( __("~P~aste"), cmcPaste, kbShiftIns, hcPaste, "Shift+Ins" )
+    +*new _TMenuItem( __("Cu~t~"), cmcCut, kbShDelete, hcCut, "Shift+Del" )
+    +*new _TMenuItem( __("~C~opy"), cmcCopy, kbCtInsert, hcCopy, "Ctrl+Ins" )
+    +*new _TMenuItem( __("~P~aste"), cmcPaste, kbShInsert, hcPaste, "Shift+Ins" )
     +*new _TMenuItem( __("~S~how clipboard"), cmShowClip, kbNoKey, hcShowClip )
     +     newLine()
-    +*new _TMenuItem( __("C~l~ear"), cmcClear, kbCtrlDel, hcClear, "Ctrl+Del" )
+    +*new _TMenuItem( __("C~l~ear"), cmcClear, kbCtDelete, hcClear, "Ctrl+Del" )
     +     newLine()
 #ifdef __DJGPP__
     +*new _TMenuItem( __("Cop~y~ to Windows Clipboard"), cmcCopyClipWin, kbNoKey, hcCopyClipWin)
@@ -139,42 +142,42 @@ TMenuBar *IDE::initMenuBar( TRect r )
     +*new _TMenuItem( __("Profile Editor"), cmcProfileEditor, kbNoKey,hcProfileEditor )
   ;
 
-  TSubMenu& search_menu = *new TSubMenu( _("~S~earch"), kbAltS, hcSearch )
+  TSubMenu& search_menu = *new TSubMenu( _("~S~earch"), kbAlS, hcSearch )
     +*new _TMenuItem( __("~F~ind..."), cmcFind, kbNoKey, hcFind )
     +*new _TMenuItem( __("~R~eplace..."), cmcReplace, kbNoKey, hcReplace )
     +*new _TMenuItem( __("~S~earch again"), cmcSearchAgain, kbNoKey, hcSearchAgain, "Ctrl+L" )
-    +*new _TMenuItem( __("~G~oto Line..."), cmGotoLine, kbNoKey, hcGotoLine, "Ctrl+J" )
-    +*new _TMenuItem( __("~J~ump to function"), cmJumpToFunction, kbAltF2, hcJumpToFunction, "Alt+F2" )
+    +*new _TMenuItem( __("~G~oto Line..."), cmGotoLine, kbCtJ, hcGotoLine, "Ctrl+J" )
+    +*new _TMenuItem( __("~J~ump to function"), cmJumpToFunction, kbAlF2, hcJumpToFunction, "Alt+F2" )
     +     newLine()
-    +*new _TMenuItem( __("~N~ext message"), cmNextMessage, kbAltF8, hcNextMessage, "Alt+F8")
-    +*new _TMenuItem( __("Previ~o~us message"), cmPrevMessage, kbAltF7, hcPrevMessage, "Alt+F7")
+    +*new _TMenuItem( __("~N~ext message"), cmNextMessage, kbAlF8, hcNextMessage, "Alt+F8")
+    +*new _TMenuItem( __("Previ~o~us message"), cmPrevMessage, kbAlF7, hcPrevMessage, "Alt+F7")
   ;
 
-  TSubMenu & run_menu = *new TSubMenu( _("~R~un"), kbAltR, hcRunMenu )
-    +*new _TMenuItem( __("~R~un"), cmRun, kbCtrlF9, hcRun, "Ctrl+F9")
+  TSubMenu & run_menu = *new TSubMenu( _("~R~un"), kbAlR, hcRunMenu )
+    +*new _TMenuItem( __("~R~un"), cmRun, kbCtF9, hcRun, "Ctrl+F9")
 #ifdef INTERNAL_DEBUGGER
     +*new _TMenuItem( __("~S~tep over"), cmStep, kbF8, hcStep, "F8" )
-    +*new _TMenuItem( __("Step over (No display swap)"), cmStepNo, kbShiftF8, hcStepNo, "Shift+F8" )
+    +*new _TMenuItem( __("Step over (No display swap)"), cmStepNo, kbShF8, hcStepNo, "Shift+F8" )
     +*new _TMenuItem( __("~T~race into"), cmTrace, kbF7, hcTrace, "F7" )
-    +*new _TMenuItem( __("Trace into (No display swap)"), cmTraceNo, kbShiftF7, hcTraceNo, "Shift+F7" )
+    +*new _TMenuItem( __("Trace into (No display swap)"), cmTraceNo, kbShF7, hcTraceNo, "Shift+F7" )
     +*new _TMenuItem( __("~G~o to cursor"), cmGoto, kbF4, hcGoto, "F4" )
-    +*new _TMenuItem( __("Go to cursor (No display swap)"), cmGotoNo, kbShiftF4, hcGotoNo, "Shift+F4" )
+    +*new _TMenuItem( __("Go to cursor (No display swap)"), cmGotoNo, kbShF4, hcGotoNo, "Shift+F4" )
     +*new _TMenuItem( __("~U~ntil return"), cmFinish, kbNoKey, hcFinish)
-    +*new _TMenuItem( __("~P~rogram reset"), cmReset, kbCtrlF2, hcReset, "Ctrl+F2" )
+    +*new _TMenuItem( __("~P~rogram reset"), cmReset, kbCtF2, hcReset, "Ctrl+F2" )
     +      newLine()
     +*new _TMenuItem( __("~M~ain function"), cmMainFunction, kbNoKey, hcMainFunction)
 #endif
     +*new _TMenuItem( __("~A~rguments ..."), cmProgArgs, kbNoKey, hcProgArgs)
   ;
 
-  TSubMenu& compile_menu = *new TSubMenu( _("~C~ompile"), kbAltC, hcCompileMenu )
-    +*new _TMenuItem( __("~C~ompile"), cmCompile, kbAltF9, hcCompile, "Alt+F9" )
+  TSubMenu& compile_menu = *new TSubMenu( _("~C~ompile"), kbAlC, hcCompileMenu )
+    +*new _TMenuItem( __("~C~ompile"), cmCompile, kbAlF9, hcCompile, "Alt+F9" )
     +*new _TMenuItem( __("~M~ake"), cmMake, kbF9, hcMake, "F9")
     +*new _TMenuItem( __("~L~ink"), cmLink, kbNoKey, hcLink)
     +*new _TMenuItem( __("~B~uild all"), cmBuild, kbNoKey, hcBuild)
   ;
 
-  TSubMenu& project_menu = *new TSubMenu( _("~P~roject"), kbAltP,hcProjectMenu )
+  TSubMenu& project_menu = *new TSubMenu( _("~P~roject"), kbAlP,hcProjectMenu )
     +*new _TMenuItem( __("Open ~p~roject"), cmOpenProject, kbNoKey, hcOpenProject )
     +*new _TMenuItem( __("~C~lose project"), cmCloseProject, kbNoKey, hcCloseProject )
     +*new _TMenuItem( __("~A~dd item"), cmAddProjectItem, kbNoKey, hcAddProjectItem )
@@ -238,7 +241,7 @@ TMenuBar *IDE::initMenuBar( TRect r )
     +*new _TMenuItem( __("~U~ser words"), cmEditUserWords, kbNoKey, hcEditUserWords)
   ;
 
-  TSubMenu& option_menu = *new TSubMenu( _("~O~ptions"), kbAltO, hcOptionsMenu )
+  TSubMenu& option_menu = *new TSubMenu( _("~O~ptions"), kbAlO, hcOptionsMenu )
     +     dir_sub
     +     c_compile_sub
     +*new _TMenuItem( __("~L~ibraries"), cmLibraries, kbNoKey, hcLibraries )
@@ -248,23 +251,23 @@ TMenuBar *IDE::initMenuBar( TRect r )
     +*new _TMenuItem( __("~S~ave options"), cmSaveOptions, kbNoKey, hcSaveOptions )
     +*new _TMenuItem( __("Loa~d~ options"), cmLoadOptions, kbNoKey, hcLoadOptions )
   ;
-  TSubMenu& window_menu = *new TSubMenu( _("~W~indows"), kbAltW, hcWindows )
-    +*new _TMenuItem( __("~S~ize/move"), cmResize, kbCtrlF5, hcResize, "Ctrl+F5" )
+  TSubMenu& window_menu = *new TSubMenu( _("~W~indows"), kbAlW, hcWindows )
+    +*new _TMenuItem( __("~S~ize/move"), cmResize, kbCtF5, hcResize, "Ctrl+F5" )
     +*new _TMenuItem( __("~Z~oom"), cmZoom, kbF5, hcZoom, "F5" )
     +*new _TMenuItem( __("~T~ile"), cmTile, kbNoKey, hcTile )
     +*new _TMenuItem( __("C~a~scade"), cmCascade, kbNoKey, hcCascade )
     +*new _TMenuItem( __("~N~ext"), cmNext, kbF6, hcNext, "F6" )
-    +*new _TMenuItem( __("~P~revious"), cmPrev, kbShiftF6, hcPrev, "Shift+F6" )
-    +*new _TMenuItem( __("~C~lose"), cmClose, kbAltF3, hcClose, "Alt+F3" )
-    +*new _TMenuItem( __("~L~ist"), cmShowWindowList, kbAlt0, hcShowWindowList, "Alt+0" )
+    +*new _TMenuItem( __("~P~revious"), cmPrev, kbShF6, hcPrev, "Shift+F6" )
+    +*new _TMenuItem( __("~C~lose"), cmClose, kbAlF3, hcClose, "Alt+F3" )
+    +*new _TMenuItem( __("~L~ist"), cmShowWindowList, kbAl0, hcShowWindowList, "Alt+0" )
     +     newLine()
     +*new _TMenuItem( __("Pro~j~ect"), cmShowProject, kbNoKey, hcShowProject)
     +*new _TMenuItem( __("~W~atches"), cmShowWatchWindow, kbNoKey, hcShowWatchWindow )
     +*new _TMenuItem( __("~M~essages"), cmShowMessages, kbNoKey, hcShowMessages)
-    +*new _TMenuItem( __("~U~serScreen"), cmUserScreen, kbAltF5, hcUserScreen, "Alt+F5" )
+    +*new _TMenuItem( __("~U~serScreen"), cmUserScreen, kbAlF5, hcUserScreen, "Alt+F5" )
   ;
 
-  TSubMenu& space_menu = *new TSubMenu( _("~ð~"), kbAltSpace )
+  TSubMenu& space_menu = *new TSubMenu( _("~ð~"), kbAlSpace )
     +*new _TMenuItem( __("about RH~I~DE"), cmAbout, kbNoKey, hcAbout )
     +*new _TMenuItem( __("~s~mall bug report"), cmBugReportSmall, kbNoKey, hcBugReportSmall)
     +*new _TMenuItem( __("~B~ug report"), cmBugReport, kbNoKey, hcBugReport)
@@ -272,27 +275,27 @@ TMenuBar *IDE::initMenuBar( TRect r )
 #ifdef __DJGPP__
     +*new _TMenuItem( __("~F~SDB"), cmFSDB, kbNoKey, hcFSDB )
 #endif
-    +*new _TMenuItem( __("~G~REP ..."), cmGREP, kbShiftF2, hcGREP, "Shift+F2" )
+    +*new _TMenuItem( __("~G~REP ..."), cmGREP, kbShF2, hcGREP, "Shift+F2" )
     +*new _TMenuItem( __("G~D~B"), cmGDB, kbNoKey, hcGDB )
     +     newLine()
     +*new _TMenuItem( __("Refresh desk~t~op"), cmRepaint, kbNoKey, hcRepaint )
     +     newLine()
-    +*new _TMenuItem( __("~C~alculator"), cmShowCalculator, kbAltF4, hcShowCalculator, "Alt+F4" )
+    +*new _TMenuItem( __("~C~alculator"), cmShowCalculator, kbAlF4, hcShowCalculator, "Alt+F4" )
     +*new _TMenuItem( __("~P~uzzle"), cmPuzzle, kbNoKey, hcPuzzle )
     +*new _TMenuItem( __("Ca~l~endar"), cmCalendar, kbNoKey, hcCalendar )
     +*new _TMenuItem( __("~A~SCII table"), cmAscii, kbNoKey, hcAscii )
   ;
 
 #ifdef INTERNAL_DEBUGGER
-  TSubMenu& debug_menu = *new TSubMenu( _("~D~ebug"), kbAltD, hcDebug )
-    +*new _TMenuItem( __("~S~et/Reset Breakpoint"), cmToggleBreak, kbCtrlF8, hcToggleBreak, "Ctrl+F8" )
-    +*new _TMenuItem( __("~E~valuate/Modify"), cmEvaluate, kbCtrlF4, hcEvaluate, "Ctrl+F4" )
-    +*new _TMenuItem( __("~W~atch an expression"), cmAddWatchEntry, kbCtrlF7, hcAddWatchEntry, "Ctrl+F7" )
-    +*new _TMenuItem( __("~I~nspect an expression"), cmInspect, kbCtrlF6, hcInspect, "Ctrl+F6" )
+  TSubMenu& debug_menu = *new TSubMenu( _("~D~ebug"), kbAlD, hcDebug )
+    +*new _TMenuItem( __("~S~et/Reset Breakpoint"), cmToggleBreak, kbCtF8, hcToggleBreak, "Ctrl+F8" )
+    +*new _TMenuItem( __("~E~valuate/Modify"), cmEvaluate, kbCtF4, hcEvaluate, "Ctrl+F4" )
+    +*new _TMenuItem( __("~W~atch an expression"), cmAddWatchEntry, kbCtF7, hcAddWatchEntry, "Ctrl+F7" )
+    +*new _TMenuItem( __("~I~nspect an expression"), cmInspect, kbCtF6, hcInspect, "Ctrl+F6" )
     +*new _TMenuItem( __("~B~reakpoints"),cmBreakPoints,kbNoKey,hcBreakPoints)
     +     newLine()
     +*new _TMenuItem( __("~D~isassembler window"),cmDisWindow,kbNoKey,hcDisWindow)
-    +*new _TMenuItem( __("~C~all stack"),cmCallStack,kbCtrlF3,hcCallStack,"Ctrl+F3")
+    +*new _TMenuItem( __("~C~all stack"),cmCallStack,kbCtF3,hcCallStack,"Ctrl+F3")
     +*new _TMenuItem( __("List of ~F~unctions"), cmFunctionList, kbNoKey, hcFunctionList)
   ;
 #endif

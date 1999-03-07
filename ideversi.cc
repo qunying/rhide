8bb7dee8b046236980075529e42af0c71c57a0b7
Robert Höhne <robert.hoehne@gmx.net>
1999-03-07 14:59:04 +0000

    *** empty log message ***

---
+++ ideversi.cc
@@ -0,0 +1,8 @@
+/* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
+/* This file is part of RHIDE. */
+/* This file is generated automatically, do not edit! */
+
+char IDEVersion[] = "RHIDE Version 1.4.7x";
+char build_date[] = __DATE__;
+char build_time[] = __TIME__;
+

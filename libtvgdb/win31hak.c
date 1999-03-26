0f917d9dd39a28245a81f9f597d666c006df8f4d
Robert Höhne <robert.hoehne@gmx.net>
1999-03-26 21:55:29 +0000

    CHanges, because the target struct is now static in the GDB sources.

--- libtvgdb/win31hak.c
+++ libtvgdb/win31hak.c
@@ -1,9 +1,5 @@
 /* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
-#ifdef WIN31_HACK
-
-int use_win31hak;
-
 #ifdef __DJGPP__
 
 #include <libgdb.h>
@@ -21,7 +17,7 @@ int use_win31hak;
 #include <dpmi.h>
 
 extern int win31; /* in librhgdb */
-extern struct target_ops go32_ops;
+static struct target_ops *go32_ops;
 int go32_insert_hw_breakpoint(CORE_ADDR,CORE_ADDR);
 int go32_remove_hw_breakpoint(CORE_ADDR,CORE_ADDR);
 
@@ -37,21 +33,38 @@ static int _win31_memory_remove_breakpoint(CORE_ADDR addr,
   return go32_remove_hw_breakpoint(addr,0 /* this is not used */);
 }
 
-static void __attribute__ ((__constructor__))
+/* Pointer to array of target architecture structures; the size of the
+   array; the current index into the array; the allocated size of the 
+   array.  */
+extern struct target_ops **target_structs;
+extern unsigned target_struct_size;
+
+void
 __win31_hack()
 {
   __dpmi_regs r;
+  unsigned i;
+  go32_ops = NULL;
   r.x.ax = 0x160a;
   __dpmi_int(0x2f,&r);
   if (r.x.ax == 0 && r.h.bh == 3) win31 = 1;
   else win31 = 0;
   if (!win31) return;
-  go32_ops.to_insert_breakpoint = _win31_memory_insert_breakpoint;
-  go32_ops.to_remove_breakpoint = _win31_memory_remove_breakpoint;
+  for (i=0; i<target_struct_size; i++)
+  {
+    if ((target_structs[i]->to_shortname) &&
+        (strcmp(target_structs[i]->to_shortname, "djgpp") == 0))
+    {
+      go32_ops = target_structs[i];
+      break;
+    }
+  }
+  if (!go32_ops)
+    return;
+  go32_ops->to_insert_breakpoint = _win31_memory_insert_breakpoint;
+  go32_ops->to_remove_breakpoint = _win31_memory_remove_breakpoint;
 }
 
 #endif /* __DJGPP__ */
-
-#endif
 
 

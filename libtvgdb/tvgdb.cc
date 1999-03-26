5623ac507057460c0459df650f4c414c109a2460
Robert Höhne <robert.hoehne@gmx.net>
1999-03-26 21:59:42 +0000

    New file to provide some common functions from libtvgdb

---
+++ libtvgdb/tvgdb.cc
@@ -0,0 +1,31 @@
+#define Uses_tvutilFunctions
+#include <libtvuti.h>
+
+#define Uses_TDataWindow
+#include <libtvgdb.h>
+
+void AddDataWindow()
+{
+  TDataWindow *data_win;
+  if ((data_win = TDataWindow::createNew()))
+    AddWindow(data_win);
+}
+
+static TDataWindow *stack_win = NULL;
+
+void ShowStackWindow()
+{
+  if (!stack_win)
+  {
+    if ((stack_win = TDataWindow::stackWindow()))
+      AddWindow(stack_win,(TWindow **) &stack_win);
+  }
+  else
+    stack_win->select();
+}
+
+void UpdateDataWindows()
+{
+  TDataWindow::updateAll();
+}
+

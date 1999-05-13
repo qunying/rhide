d349ea60243af2319bf781500f052059a4e3e3f9
Robert Höhne <robert.hoehne@gmx.net>
1999-05-13 20:33:28 +0000

    Allow the debugged program being signalled also for DJGPP >= 2.03

--- librhgdb/gdbinter.c
+++ librhgdb/gdbinter.c
@@ -73,7 +73,7 @@ DEBUG("|signal_string_end|");
   else
     _UserWarning(WARN_SIGNALED,signal);
   // An attempt to recover after SIGINT
-  #ifndef __DJGPP__
+#if !defined(__DJGPP__) || __DJGPP__ > 2 || (__DJGPP__ >= 2 && __DJGPP_MINOR__ >= 3)
     if (     strncmp(signal,"SIGINT," ,7)==0
           || strncmp(signal,"SIGFPE," ,7)==0
           || strncmp(signal,"SIGABRT,",8)==0
@@ -86,9 +86,9 @@ DEBUG("|signal_string_end|");
       {
           call_reset=1;
       }
-  #else
+#else
   call_reset = 1;
-  #endif
+#endif
 }
 
 void

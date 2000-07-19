44b01b1f21676fa1f600f5ef9d7b42880c4078fc
Robert Höhne <robert.hoehne@gmx.net>
2000-07-19 19:20:39 +0000

    *** empty log message ***

--- ideversi.cc
+++ ideversi.cc
@@ -2,7 +2,7 @@
 /* This file is part of RHIDE. */
 /* This file is generated automatically, do not edit! */
 
-char IDEVersion[] = "RHIDE Version 1.4.8";
+char IDEVersion[] = "RHIDE Version 1.4.9";
 char build_date[] = __DATE__;
 char build_time[] = __TIME__;
 

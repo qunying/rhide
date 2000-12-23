e96efeb1d98940e13e3038a848997befc6253662
Robert Höhne <robert.hoehne@gmx.net>
2000-12-23 12:36:08 +0000

    Add the -Os flag

--- include/flags/opt_opt.h
+++ include/flags/opt_opt.h
@@ -1,6 +1,7 @@
 /* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
 F("-O%d?")
+F("-Os")
 F("-ffloat-store")
 F("-fno-float-store")
 F("-fdefault-inline")

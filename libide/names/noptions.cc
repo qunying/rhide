ed1dab29362092f22ea73d2de42b2190e7a06738
Robert Höhne <robert.hoehne@gmx.net>
2001-09-01 21:40:23 +0000

    include only tv.h for the n() macro

--- libide/names/noptions.cc
+++ libide/names/noptions.cc
@@ -1,7 +1,6 @@
 /* Copyright (C) 1996-2000 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
 #define Uses_n
-#include <ttypes.h>
-#include <tvutil.h>
+#include <tv.h>
 
 n(TOptions)
--- libtvuti/names/nparamli.cc
+++ libtvuti/names/nparamli.cc
@@ -3,7 +3,6 @@
 /* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
 #define Uses_n
-#include <ttypes.h>
-#include <tvutil.h>
+#include <tv.h>
 
 n(TParamList)

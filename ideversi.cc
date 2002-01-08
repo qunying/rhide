8446f67cf0aeadcc96192fa2aedbb5ee2ba595d7
Robert Höhne <robert.hoehne@gmx.net>
2002-01-08 20:49:28 +0000

    this is an generated file

--- ideversi.cc
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
-/* This file is part of RHIDE. */
-/* This file is generated automatically, do not edit! */
-
-char IDEVersion[] = "RHIDE 1.4.9";
-char build_date[] = __DATE__;
-char build_time[] = __TIME__;
-

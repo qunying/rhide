7c2bc48a18bb3a079974c92d9daec084d5bc26c0
Robert Höhne <robert.hoehne@gmx.net>
2002-02-09 12:30:02 +0000

    providing dummy iconv functionallity to reduce executable size when using gettext which needs iconv

---
+++ iconv.c
@@ -0,0 +1,44 @@
+#ifdef USE_DUMMY_ICONV
+
+int iconv_open(const char* tocode __attribute__((unused)), 
+               const char* fromcode __attribute__((unused)))
+{
+  return -1;
+}
+
+int libiconv_open(const char* tocode, 
+                  const char* fromcode)
+{
+  return iconv_open(tocode, fromcode);;
+}
+
+
+int iconv(int cd __attribute__((unused)),
+          const char **inbuf __attribute__((unused)),
+	  int *inbytesleft __attribute__((unused)),
+	  char **outbuf __attribute__((unused)),
+	  int *outbytesleft __attribute__((unused)))
+{
+  return 0;
+}
+
+int libiconv(int cd,
+             const char **inbuf,
+	     int *inbytesleft,
+	     char **outbuf,
+	     int *outbytesleft)
+{
+  return iconv(cd, inbuf, inbytesleft, outbuf, outbytesleft);
+}
+
+int iconv_close(int cd __attribute__((unused)))
+{
+  return -1;
+}
+
+int libiconv_close(int cd)
+{
+  return iconv_close(cd);
+}
+	    
+#endif
